0bb431583359e08a951678460e608e15193a7d20
Siân <66971939+sian-codes@users.noreply.github.com>
2020-11-17 22:31:28 +0000

    Update index.js

--- Dog Human Age Calc/index.js
+++ Dog Human Age Calc/index.js
@@ -1,9 +1,11 @@
-/*Dog Age to Human Age Calculator using what I've learned in the above 101
-sections. Code needs to create a prompt to ask user for dogs age, calculate the
+/*Dog Age to Human Age Calculator, code needs to create a prompt to ask user for dogs age, calculate the
 age of the dog using the formula humanAge=(dogAge-2)x4+21 and provide the
 anwser as an alert for the user */
+
 function dogHumanAge(dogAge){
+  
 var dogAge=prompt("How old is your Dog?");
 var humanAge=((dogAge-2)x4)+21;
 alert("Your dog is "+humanAge+"in human years!");
+  
 }
