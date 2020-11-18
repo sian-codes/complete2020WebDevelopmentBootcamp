0ae55a7d2edba453225bb1056fd1cca0d28d9561
Siân <66971939+sian-codes@users.noreply.github.com>
2020-11-18 22:26:38 +0000

    Delete Who is buying lunch.js

--- Who is buying lunch.js
+++
@@ -1,6 +0,0 @@
-function whosPaying(names) {
-    var numberOfPeople = names.length;
-    var randomPersonPosition = Math.floor(Math.random()*numberOfPeople);
-    var randomPerson = names[randomPersonPosition];
-
-    return randomPerson + " is going to buy lunch today!";
