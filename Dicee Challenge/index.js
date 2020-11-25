e10a4d06b337250af5f82db32911e097cf145666
Siân <66971939+sian-codes@users.noreply.github.com>
2020-11-25 06:04:23 +0000

    Update index.js

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,3 +1,20 @@
-// Creating a random number between 1 and 6
-var randomNumber1;
-randomNumber1=Math.floor(Math.random()*6)+1; 
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //creates a random num 1-6
+var randomDiceImg = "dice" + randomNumber1 + ".png"; //creates a string for dice imgs
+var randomImageSource = "images/" + randomDiceImg; // images/dice6.png
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; //creates a random num 1-6
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//which player wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
