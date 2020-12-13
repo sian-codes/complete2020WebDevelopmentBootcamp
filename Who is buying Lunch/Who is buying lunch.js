9a4c336749e2f9a1a40702da1ea3768eadcc4fb4
Siân <66971939+sian-codes@users.noreply.github.com>
2020-12-13 21:16:54 +0000

    Update Who is buying lunch.js

--- Who is buying Lunch/Who is buying lunch.js
+++ Who is buying Lunch/Who is buying lunch.js
@@ -1,4 +1,4 @@
-function whosPaying(names) {
+function whosPaying(names) { //using arrarys to get a random name from a list of names
     var numberOfPeople = names.length;
     var randomPersonPosition = Math.floor(Math.random()*numberOfPeople);
     var randomPerson = names[randomPersonPosition];
