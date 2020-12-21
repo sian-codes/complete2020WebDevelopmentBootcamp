13b81f312ab8220db291bead9ed39bb3cd4ec50c
Siân <66971939+sian-codes@users.noreply.github.com>
2020-12-21 22:01:04 +0000

    Update index.js

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,2 +1,9 @@
 $("h1").addClass("big-title"); //adding class via jQuery
-$("button");
+//when buttons are clicked h1 fades out
+$("button").on("click", function(){
+  $("h1").fadeOut();
+});
+//when buttons are clicked h1 slides up
+$("button").on("click", function(){
+  $("h1").slideUp();
+});
