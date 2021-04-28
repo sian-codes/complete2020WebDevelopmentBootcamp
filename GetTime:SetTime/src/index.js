5475a83bbc4a73058603b1b88e6829930ac3ca79
Siân <66971939+sian-codes@users.noreply.github.com>
2021-04-28 21:27:28 +0100

    Use useState Hook to set time

---
+++ GetTime:SetTime/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//Challenge:
+
+//2. Given that you can get code to be called every second
+//using the setInterval method.
+//Can you get the time in your <h1> to update every second?
+
+//e.g. uncomment the code below to see Hey printed every second.
+// function sayHi() {
+//   console.log("Hey");
+// }
+// setInterval(sayHi, 1000);
